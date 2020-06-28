6bbf6874d461976bc204edc46256df2d6d027d67
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-06-28 21:34:11 +0900

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,23 @@ class PostsController < ApplicationController
 
   def index
     @posts= Post.all
+
+    # ーーー追加ここからーーー
+    wday = DateTime.now.wday  # 曜日（0:日曜日、 1:月曜日...6:土曜日）
+    hour = DateTime.now.hour  # 時間
+    minute = DateTime.now.minute  # 分
+
+    if wday == 0 && hour == 21 && minute == 00  # 日曜日21時00分に実行
+      unattached_files = ActiveStorage::Blob.unattached  ## 記事と結びついていないファイル
+      seven_days_ago = DateTime.now.ago(7.day)  # 現在の日時から1週間前
+      # 作成されたのが1週間以上前で記事と結びついていないファイルを取得
+      unattached_files.where("active_storage_blobs.created_at <= ?", seven_days_ago).each do |file|
+        ## データベースから削除する
+        file.purge
+      end
+    end
+    # ーーー追加ここまでーーー
+
   end
 
   def new
