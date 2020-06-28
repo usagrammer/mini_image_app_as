5ed945c4d7f3e159bdc5e05d41895f043019c273
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-06-28 16:05:33 +0900

    Update post_form.js

--- app/javascript/packs/post_form.js
+++ app/javascript/packs/post_form.js
@@ -8,6 +8,30 @@ document.addEventListener("turbolinks:load", function () {
   const imageFileField = document.querySelector('#post_images'); // file_field
   const imageSelectButton = document.querySelector('#image-select-button'); // 画像選択ボタン
 
+  let selectedImageBlobId = -1; // 選択した画像のblob_id（-1の場合は新規画像）
+
+  const clickedEditButton = (e) => { // 変更ボタンをクリックした時に実行される処理
+    console.log(e.target);
+
+    const editButton = e.target; // e.targetにクリックされた削除ボタンが格納されている
+    const previewWrapper = editButton.closest('.image-preview'); // プレビュー画像とボタンたちの祖先要素を取得する
+
+    selectedImageBlobId = previewWrapper.getAttribute('data-image-blob-id');
+    console.log(selectedImageBlobId);
+
+    imageFileField.click();
+  };
+
+  imageSelectButton.addEventListener('click', () => {
+    selectedImageBlobId = -1;
+    console.log(selectedImageBlobId);
+  });
+
+  const editButtons = document.querySelectorAll('.image-edit-button');
+  editButtons.forEach((button) => { // 変更ボタンにイベントを設定していく
+    button.addEventListener('click', clickedEditButton);
+  });
+
   const clickedDeleteButton = (e) => { // 削除ボタンをクリックした時に実行される処理
     console.log(e.target);
 
@@ -17,13 +41,19 @@ document.addEventListener("turbolinks:load", function () {
     const deleteImageBlobId = previewWrapper.getAttribute('data-image-blob-id');
     console.log(deleteImageBlobId);
 
-    const deleteImageBlobIdElement = // 削除したい画像のidをparamsに送るための要素
-      `
+    const addImageHidden = document.querySelector(`input[data-blob-id="${deleteImageBlobId}"]`); // 追加するための要素があるか確認
+    if (addImageHidden) {
+      // 追加するためのhidden_fieldがある場合、削除する
+      addImageHidden.remove();
+    } else {
+      // 追加するためのhidden_fieldがない場合、削除するためのhidden_fieldを追加する
+      const deleteImageBlobIdElement = // 削除したい画像のidをparamsに送るための要素
+        `
         <input type="hidden" name="post[delete_image_blob_ids][]" value="${deleteImageBlobId}">
       `;
-    const form = document.querySelector('form');
-    form.insertAdjacentHTML("beforeend", deleteImageBlobIdElement); // deleteImageBlobIdElementをビューに追加
-    // --- 追加ここまで ----
+      const form = document.querySelector('form');
+      form.insertAdjacentHTML("beforeend", deleteImageBlobIdElement); // deleteImageBlobIdElementをビューに追加
+    }
 
     previewWrapper.remove();
   }
@@ -50,7 +80,7 @@ document.addEventListener("turbolinks:load", function () {
 
         const newImageFile =
           `
-          <input type="hidden" name="post[new_images][]" value="${blob.signed_id}">
+          <input type="hidden" name="post[new_images][]" value="${blob.signed_id}" data-blob-id="${blob.id}">
         `;
         const form = document.querySelector('form');
         form.insertAdjacentHTML("beforeend", newImageFile);
@@ -68,12 +98,24 @@ document.addEventListener("turbolinks:load", function () {
               </div>
             </div>
           `;
-        imageSelectButton.insertAdjacentHTML("beforebegin", previewHtml);
-        // --- 追加ここから ----
+
+        if (selectedImageBlobId == -1) {
+          // -1のとき=画像の新規追加
+          console.log('画像を追加します');
+          imageSelectButton.insertAdjacentHTML("beforebegin", previewHtml);
+        } else {
+          // -1でないとき=画像の変更
+          console.log('画像を変更します');
+          // --- 追加ここから ----
+          // 既存のプレビュー要素
+          const oldPreview = document.querySelector(`.image-preview[data-image-blob-id="${selectedImageBlobId}"]`);
+          oldPreview.outerHTML = previewHtml; // 既存のプレビューを新しいプレビューで書き換え
+          // --- 変更ここまで ----
+        }
+
         const insertedPreview = document.querySelector(`.image-preview[data-image-blob-id="${blob.id}"]`);
         const insertedDeleteButton = insertedPreview.querySelector('.image-delete-button');
-        insertedDeleteButton.addEventListener('click', clickedDeleteButton);
-        // --- 追加ここまで ----
+        insertedDeleteButton.addEventListener('click', clickedDeleteButton); // 後から追加した削除ボタンにイベントを追加
       }
     })
 
